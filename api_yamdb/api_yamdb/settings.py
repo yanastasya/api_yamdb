39c586945a1981c99283dd63ff3830d6e7068dad
Анастасия Клинцова <yanastasya1@yandex.ru>
2022-11-15 14:16:45 +0300

    готово к слиянию с develop

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'reviews.apps.ReviewsConfig',
-    'api.apps.ApiConfig',    
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+"""+ модель юзера."""
+
 
 class Genre(models.Model):
     """Категории жанров."""
