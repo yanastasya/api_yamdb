cbec400fc8c86bef00fffcfa0f6a5ce637185d58
Анастасия Клинцова <yanastasya1@yandex.ru>
2022-11-15 12:40:30 +0300

    созданы и зарегистрированны  приложения api и rewiever

---
+++ api_yamdb/reviews/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+
+
+class Genre(models.Model):
+    """Категории жанров."""
+    pass
+
+
+class Categories(models.Model):
+    """Категории произведений."""
+    pass
+
+
+class Titles(models.Model):
+    """Произведения, к которым пишут отзывы.
+    Определённый фильм, книга или песенка."""
+    pass
+
+
+class Rewiews(models.Model):
+    """Отзывы к произведениям."""
+    pass
+
+
+class Comments(models.Model):
+    """Комментарии к отзывам."""
+    pass
