d235d8a462c423145f419d4d16a880bcfb8d3505
yanastasya <109209321+yanastasya@users.noreply.github.com>
2022-11-17 16:17:06 +0300

    Update models.py

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -8,35 +8,51 @@ class Genre(models.Model):
     pass
 
 
-class Categories(models.Model):
+class Categorie(models.Model):
     """Категории произведений."""
     pass
 
 
-class Titles(models.Model):
+class Title(models.Model):
     """Произведения, к которым пишут отзывы.
     (определённый фильм, книга или песенка).
     """
+    name = models.CharField(
+        verbose_name="название произведения",
+        max_length=200,
+    )
     category = models.ForeignKey(
-        Categories,
+        Categorie,
+        verbose_name="Категория",        
         related_name='titles',
-        on_delete=models.CASCADE,
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
     )
-    genre = models.ForeignKey(
-        Genre,
-        related_name='titles',
-        on_delete=models.CASCADE,
+    genre = models.ManyToManyField(
+        Genre,        
+        verbose_name="Жанр",
+        related_name='titles',               
+        through='TitleGenre',
+        null=True,
+        blank=True,
     )
-    name = models.CharField(max_length=200)
-    year = models.IntegerField()
-    description = models.TextField(blank=True, null=True)
+    year = models.IntegerField(
+        verbose_name="год создания произведения",
+    )
+    description = models.TextField(
+        verbose_name="описание произведения",
+    )
 
+    def __str__(self):
+        return self.name
+    
 
-class Rewiews(models.Model):
+class Rewiew(models.Model):
     """Отзывы к произведениям."""
     pass
 
 
-class Comments(models.Model):
+class Comment(models.Model):
     """Комментарии к отзывам."""
     pass
